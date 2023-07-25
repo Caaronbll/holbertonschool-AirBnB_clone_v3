bd63e7b6f6de603a437be756907f37b514ac6330
Night8Guy <robert@lionsroad.com>
2023-07-25 17:41:16 -0500

    documentation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,10 @@ from flask import Blueprint
 # Creates an instance Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+# Import views for each endpoint (index, states, cities,
+# amenities, places, users, places_reviews)
+# This allows organizing the views in separate files
+#  for better readability and maintainability.
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
